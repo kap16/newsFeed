61ff8d06c51bdbf1a77ef0e1fea44e198e578ba4
kap16 <kap.16@outlook.com>
2019-03-04 22:29:19 +0000

    updated item model

--- server/models/item.js
+++ server/models/item.js
@@ -5,20 +5,19 @@ const ItemSchema = new Schema({
     title:{ type: String, required: true},
     link: { type: String },
     pubDate: { type: Date },
-    creator: { type: String },
+    description: { type: String },
     content: { type: String },
-    contentSnippet: { type: String },
-    guid: { type: String },
+    guid: { type: Object },
     categories: [{ type: String }],
-    isoDate: { type: Date },
 
     // database related info
     flagged: { type: Boolean, default: false },
     favourite: { type: Boolean, default: false },
+    read: { type: Boolean, default: false },
     createdOn: { type: Date, required:true },
     createdBy: { type: Schema.Types.ObjectId, ref: 'User', required:true },
     updatedOn: { type: Date },
-    source: { type: String},
+    source: { type: Schema.Types.ObjectId, ref: 'Source', required:true },
     deleted: { type: Boolean, default: false },
     deletedOn: { type: Date, default: null }
 });
